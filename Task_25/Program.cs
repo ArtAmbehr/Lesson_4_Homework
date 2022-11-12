5b448c50f82e204320dca39306f61b8ff44b4a06
Art <art-75@yandex.ru>
2022-11-12 15:14:29 +0300

    Task_25_final

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -2,4 +2,22 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе натуральное число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int res = numberA;
+Console.WriteLine($"Число {numberA}, возведенное в степень {numberB}, равно {GetPower(res)} ");
 
+int GetPower(int result)
+{
+int count = 1;
+int res = numberA;
+
+    while (count < numberB)
+    {
+        result = result * numberA;
+        count++;
+    }
+    return result;
+}
