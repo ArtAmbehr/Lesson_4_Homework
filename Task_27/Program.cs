e90834e1abaaaa60691081ed6eb77b67637ca19f
Art <art-75@yandex.ru>
2022-11-12 15:19:15 +0300

    Task_27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -3,3 +3,18 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {num} равна {GetSumDigits(num)} ");
+
+int GetSumDigits(int number)
+{
+    int sum = 0;
+    while (number>0)
+    {
+        int digit = number % 10;
+        sum = sum + digit;
+        number = number / 10;   
+    }
+        return sum;
+}
